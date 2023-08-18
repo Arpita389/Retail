64299d50e3c151fb525c69cd846928498772f521
Dhanada Panda <130206432+Dhanada-Panda@users.noreply.github.com>
2023-08-18 12:09:45 +0530

    cart.js

--- cart.js
+++ cart.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-
+import "./cart.css";
 function CartPage() {
   const [cartItems, setCartItems] = useState([]);
 
@@ -23,7 +23,7 @@ function CartPage() {
       );
       const data = await response.json();
       console.log(data);
-      getCartItems(); // Refresh the cart items after deleting
+      getCartItems(); 
     } catch (error) {
       console.log(error);
     }
@@ -37,10 +37,10 @@ function CartPage() {
     <div>
       <h2>Cart Items</h2>
       <ul>
-        {cartItems.map((item) => (
-          <li key={item.id}>
+        {cartItems.map((item, index) => (
+          <li key={index}>
             {item}
-            <button onClick={() => handleDeleteItem(item)}>Delete</button>
+            <button id="cart-button" onClick={() => handleDeleteItem(item)}>Delete</button>
           </li>
         ))}
       </ul>
