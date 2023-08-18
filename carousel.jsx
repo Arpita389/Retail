d74a710d39423c044969e76a270ccca238fb5066
Arpita389 <138769882+Arpita389@users.noreply.github.com>
2023-08-18 17:28:10 +0530

    Add files via upload

---
+++ carousel.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Carousel from "react-material-ui-carousel";
+import Item from "./item";
+import slider from "../helper/slider.json";
+function Example() {
+  return (
+    <Carousel>
+      {slider.map((item) => (
+        <Item key={item.id} item={item} />
+      ))}
+    </Carousel>
+  );
+}
+export default Example;
