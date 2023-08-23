555d55790326a9b3ee1087420b12465c138ed0d5
Dhanada Panda <130206432+Dhanada-Panda@users.noreply.github.com>
2023-08-23 14:24:25 +0530

    usertype.jsx updated

--- usertype.jsx
+++ usertype.jsx
@@ -19,7 +19,17 @@ const theme = extendTheme({
 function UserTypeSelection() {
   return (
     <ChakraProvider theme={theme}>
-      <Box textAlign="center" p={10}>
+      <Box
+        textAlign="center"
+        p={10}
+        style={{
+          backgroundImage:
+            'url("https://img.freepik.com/premium-photo/shopping-making-purchase-concept-sale-discount_185193-129894.jpg?w=2000")',
+          backgroundSize: "cover",
+          backgroundRepeat: "no-repeat",
+          minHeight: "100vh",
+        }}
+      >
         <Text fontSize="xl" mb={6}>
           Choose your user type:
         </Text>
